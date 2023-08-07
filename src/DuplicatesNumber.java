713ff57d8450dd5fee054bf85e807e04f9a182d7
manishpaudel07 <47307595+manishpaudel07@users.noreply.github.com>
2023-08-07 15:31:01 -0400

    duplicates number exist or not

--- src/DuplicatesNumber.java
+++ src/DuplicatesNumber.java
@@ -1,6 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class DuplicatesNumber {
 
+	 public boolean containsDuplicate(int[] nums) {
+	        
+	        Set<Integer> set=new HashSet<>();
+	        
+	        for (int i=0;i<nums.length;i++)
+	        {
+	            if (!set.add(nums[i]))
+	            {
+	                return true;
+	            }            
+	        }
+	        
+	        return false;
+	 }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
